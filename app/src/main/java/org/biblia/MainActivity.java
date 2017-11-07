96babb0008da0384c9363d1f39ce78179fcf7614
jlozano871 <jose.lozano.tamayo@gmail.com>
2017-11-06 20:00:26 -0500

    inicio app class

--- app/src/main/java/org/biblia/MainActivity.java
+++ app/src/main/java/org/biblia/MainActivity.java
@@ -3,6 +3,9 @@ package org.biblia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Activity main, inicio del app
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override

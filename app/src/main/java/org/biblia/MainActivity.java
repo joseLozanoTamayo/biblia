f212547ffa76bb48cd0b2f00a1025725a4fdfe51
jlozano871 <jose.lozano.tamayo@gmail.com>
2017-11-06 21:47:17 -0500

    inicio app class, add menu inicial

--- app/src/main/java/org/biblia/MainActivity.java
+++ app/src/main/java/org/biblia/MainActivity.java
@@ -2,15 +2,24 @@ package org.biblia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 /**
  * Activity main, inicio del app
  */
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }

1d4a6b609062d67b1a0b0e1b80ebad640e85734c
jlozano871 <jose.lozano.tamayo@gmail.com>
2017-11-13 20:24:15 -0500

    add evento a fragment to view

--- app/src/main/java/org/biblia/AntiguoTestamento.java
+++ app/src/main/java/org/biblia/AntiguoTestamento.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.biblia.R;
 
@@ -15,12 +18,35 @@ import org.biblia.R;
 
 public class AntiguoTestamento extends Fragment {
 
+    private Button boton;
+    private EditText edit;
+    private TextView text;
+
     //Constructor default
     public AntiguoTestamento(){};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View antiguoTestamento = inflater.inflate(R.layout.antiguo_testamento, container, false);
+        View antiguoTestamento = inflater
+                .inflate(R.layout.antiguo_testamento, container, false);
+
+        boton = (Button)antiguoTestamento.findViewById(R.id.main_button);
+        edit  = (EditText)antiguoTestamento.findViewById(R.id.main_editText);
+        text  = (TextView)antiguoTestamento.findViewById(R.id.main_textview);
+
+        text.setText("TEXT NEW");
+
+        boton.setOnClickListener( new View.OnClickListener() {
+
+            public void onClick(View view){
+
+                String mensaje = edit.getText().toString();
+                text.setText(String.valueOf(mensaje.length()));
+
+            }
+
+        });
+
         return antiguoTestamento;
     }
 }
